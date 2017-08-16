99c7298ecf824612d10f4e4f7ac94906ac648d86
acarlos94 <antoniocarlos048@gmail.com>
2017-08-15 23:22:54 -0300

    Crud Instituicao

---
+++ src/model/beans/Instituicao.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.beans;
+
+/**
+ *
+ * @author Antonio
+ */
+public class Instituicao {
+    
+    private int codInstituicao;
+    private String nomeInstituicao;
+
+    public int getCodInstituicao() {
+        return codInstituicao;
+    }
+
+    public void setCodInstituicao(int codInstituicao) {
+        this.codInstituicao = codInstituicao;
+    }
+
+    public String getNomeInstituicao() {
+        return nomeInstituicao;
+    }
+
+    public void setNomeInstituicao(String nomeInstituicao) {
+        this.nomeInstituicao = nomeInstituicao;
+    }
+    
+    
+    
+}
